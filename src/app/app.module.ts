8fc5dae2e04523f261357d707fe0d54bba1fa370
kavidiuk <kaviduwarnakulasuriya@gmail.com>
2023-12-11 09:20:58 +0100

    U3S3G5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { RouterModule, Route } from '@angular/router';
+
+const routes:Route[]=[
+  {
+    path:'',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
